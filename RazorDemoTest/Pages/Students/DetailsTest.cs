eaafcc8d33c8ae0eda48cbafdccf254004100a47
Hinault <ngdonfack@gmail.com>
2017-11-18 06:55:30 -0500

    Update DetailsTest

--- RazorDemoTest/Pages/Students/DetailsTest.cs
+++ RazorDemoTest/Pages/Students/DetailsTest.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RazorDemo.Pages.Students;
+using System.Threading.Tasks;
 
 namespace RazorDemoTest.Pages.Students
 {
-    class DetailsTest
+    [TestClass]
+    public class DetailsTest : BaseTest
     {
+
+        [TestMethod]
+        public async Task OnGetAsync_ReturnStudent()
+        {
+            //Arrange
+            var detailsModel = new DetailsModel(Context);
+
+            //Act
+            await detailsModel.OnGetAsync(3);
+
+            //Assert
+            var student = detailsModel.Student;
+            Assert.IsNotNull(student);
+            Assert.AreEqual(3, student.Id);
+            Assert.AreEqual("Derosi", student.FirstName);
+            Assert.AreEqual("Ronald", student.LastName);
+            Assert.AreEqual("r.derosi@gmail.com", student.Email);
+
+        }
+
+        [TestMethod]
+        public async Task OnGetAsync_ReturnNotFound_WithNullId()
+        {
+            //Arrange
+            var detailsModel = new DetailsModel(Context);
+
+            //Act
+            IActionResult actionResult =  await detailsModel.OnGetAsync(null);
+
+            //Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+            Assert.IsNull(detailsModel.Student);
+        }
+
+
+        [TestMethod]
+        public async Task OnGetAsync_ReturnNotFound_WithId()
+        {
+            //Arrange
+            var detailsModel = new DetailsModel(Context);
+
+            //Act
+            IActionResult actionResult = await detailsModel.OnGetAsync(6);
+
+            //Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+            Assert.IsNull(detailsModel.Student);
+        }
     }
 }
